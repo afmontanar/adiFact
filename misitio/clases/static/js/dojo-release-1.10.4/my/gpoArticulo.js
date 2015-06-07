11c33b28504581f6e266223da6cb1a13324a8e48
Andres <afmontanar@gmail.com>
2015-06-07 17:30:59 -0500

    arreglando todos los layouts voy a culminar la busqueda de los articulos

--- misitio/clases/static/js/dojo-release-1.10.4/my/gpoArticulo.js
+++ misitio/clases/static/js/dojo-release-1.10.4/my/gpoArticulo.js
@@ -18,9 +18,45 @@ define(["dojo/_base/declare", 'dijit/Dialog', 'dijit/form/ValidationTextBox'], f
     constructor: function(){
        this.gpoarticulo = new Dialog({
         title: "Actualizacion de articulos",
-        content: "<form><legend style='font-weight: bold;'>Datos del articulo</legend><div class='artig1'><label for='noma'>Nombre</label><input type='text' name='nomar' value='' id='nomar'></input></div><div class='artig'><label for='codia'>Codigo</label><input type='text' name='codiar' value='' id='codiar'></input></div><div class='artig5'><label for='nomplul'>Nombre plu</label><input type='text' name='nomplu' value='' id='nomplu'></input></div><div class='artig4'><label for='codplu'>Codigo plu</label><input type='text' name='codplu' value='' id='codplu'></input></div><div class='artig3'><label for='ticoinv'>Tipo de inventario</label><input type='text' name='tipinv' value='' id='tipinv'></input></div><div class='artig2'><label for='ticod'>Tipo de codigo</label><input type='text' name='ticodr' value='' id='ticodr'></input></div></form>",
+        content: "<form><legend style='font-weight: bold;'>Datos del articulo</legend><div class='artig1'><label for='nomag'>Nombre</label><input type='text' name='nomag' value='' id='nomag'></input></div><div class='artig'><label for='codiag'>Codigo</label><input type='text' name='codiag' value='' id='codiag'></input></div><div class='artig5'><label for='nomplulg'>Nombre plu</label><input type='text' name='nomplug' value='' id='nomplug'></input></div><div class='artig4'><label for='codplug'>Codigo plu</label><input type='text' name='codplug' value='' id='codplug'></input></div><div class='artig3'><label for='ticoinv'>Tipo de inventario</label><input type='text' name='tipinvg' value='' id='tipinvg'></input></div><div class='artig2'><label for='ticodg'>Tipo de codigo</label><input type='text' name='ticodg' value='' id='ticodg'></input></div></form>",
         style: "width: 1000px;"
     });
+
+        var nomag = new dijit.form.TextBox({
+            name: "nomag",
+            value: "" /* no or empty value! */,
+            placeHolder: "",
+            style: "width: 30em;"
+        }, "nomag");
+
+        var codiag = new dijit.form.TextBox({
+            name: "codiag",
+            value: "" /* no or empty value! */,
+            placeHolder: "",
+            style: "width: 10em;"
+        }, "codiag");
+
+        var codplug = new dijit.form.TextBox({
+            name: "codplug",
+            value: "" /* no or empty value! */,
+            placeHolder: "",
+            style: "width: 2em;"
+        }, "codplug");
+
+        var tipinvg = new dijit.form.TextBox({
+            name: "tipinvg",
+            value: "" /* no or empty value! */,
+            placeHolder: "",
+            style: "width: 2em;"
+        }, "tipinvg");
+
+        var ticodg = new dijit.form.TextBox({
+            name: "ticodg",
+            value: "" /* no or empty value! */,
+            placeHolder: "",
+            style: "width: 2em;"
+        }, "ticodg");
+
       
    }
   });
